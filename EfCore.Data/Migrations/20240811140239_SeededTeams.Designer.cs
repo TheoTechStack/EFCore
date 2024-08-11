52da419b9f7ac9920790bea8e5a6ac56cb0015ec
TheoTechStack <130162778+TheoTechStack@users.noreply.github.com>
2024-08-11 16:17:50 +0200

    Seed Teams Model

--- EfCore.Data/Migrations/20240811140239_SeededTeams.Designer.cs
+++ EfCore.Data/Migrations/20240811140239_SeededTeams.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EfCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EfCore.Data.Migrations
 {
     [DbContext(typeof(FootballLeagueDbContext))]
-    partial class FootballLeagueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240811140239_SeededTeams")]
+    partial class SeededTeams
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
